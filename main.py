de6ae964a395e85832a0eaab6a49e552f3fd6345
Nikita Gordonov <104985817+N1kesha@users.noreply.github.com>
2023-11-13 15:07:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,36 +12,41 @@ import plotly.express as px
 with st.echo(code_location='below'):
     st.title('COVID-19 worldwide data visualization')
 
-    @st.cache(allow_output_mutation=True)
+
+    @st.cache_data()
     def get_data():
         data_url = "covid_data_cleaned.csv"
         return pd.read_csv(data_url)
+
 
     df = get_data()
     df1 = pd.read_excel('time_series_covid19_confirmed_global.xls', engine='xlrd')
     df['date'] = pd.to_datetime(df['date'], dayfirst=True)
     df = df.dropna(subset=['total_cases', 'total_deaths'])
-    df = df.drop(["total_vaccinations", "new_cases", "new_deaths", "total_boosters", "new_vaccinations", "people_vaccinated"], axis=1)
+    df = df.drop(
+        ["total_vaccinations", "new_cases", "new_deaths", "total_boosters", "new_vaccinations", "people_vaccinated"],
+        axis=1)
 
-    df['deaths/cases, %'] = df['total_deaths']/df['total_cases']*100
-    df['deaths/vaccines'] = df['total_deaths']/df['people_fully_vaccinated']*100
+    df['deaths/cases, %'] = df['total_deaths'] / df['total_cases'] * 100
+    df['deaths/vaccines'] = df['total_deaths'] / df['people_fully_vaccinated'] * 100
     countries = list(df['location'].unique())
     continents = list(df['continent'].unique())
     continents = [continent for continent in continents if not (pd.isnull(continent)) == True]
 
     lists = []
     for country in countries:
-        x = np.array(df[df['location']==country]['date']).reshape((-1,1))
-        y = df[df['location']==country]['deaths/cases, %']
+        x = np.array(df[df['location'] == country]['date']).reshape((-1, 1))
+        y = df[df['location'] == country]['deaths/cases, %']
         model = LinearRegression().fit(x, y)
-        lists.append([country, round(float(100000000000000000*model.coef_), 2)])
+        lists.append([country, round(float(100000000000000000 * model.coef_), 2)])
 
     pares = pd.DataFrame(lists, columns=['country', 'slope'])
     df = df.merge(pares, left_on='location', right_on='country').drop(['country'], axis=1)
 
     triple = []
     for country in countries:
-        triple.append([country, df[df['location']==country]['continent'].unique()[0], df[df['location']==country]['slope'].unique()[0]])
+        triple.append([country, df[df['location'] == country]['continent'].unique()[0],
+                       df[df['location'] == country]['slope'].unique()[0]])
     triple_df = pd.DataFrame(triple, columns=['country', 'continent', 'slope']).dropna()
 
     """
@@ -50,8 +55,9 @@ with st.echo(code_location='below'):
     The following plot allows you to visually compare what percentage of sick with COVID-19 people died in any countries.
     """
 
-    options_countries = st.multiselect("Choose as many countries (or continents) as you want to plot. You can also type its name in the line.",
-                                       countries)
+    options_countries = st.multiselect(
+        "Choose as many countries (or continents) as you want to plot. You can also type its name in the line.",
+        countries)
     alt.data_transformers.disable_max_rows()
     df_plot = df[df['location'].isin(options_countries)]
 
@@ -75,7 +81,7 @@ with st.echo(code_location='below'):
     ).transform_filter(nearest)
 
     st.altair_chart(alt.layer(
-    line, selectors, points, rules, text
+        line, selectors, points, rules, text
     ).properties(width=600, height=500).interactive())
 
     ### END FROM
@@ -84,7 +90,7 @@ with st.echo(code_location='below'):
     Knowing how graphs for all countries look like, we can build a simple linear regression to 
     compare how effectively and decisively countries have been fighting COVID-19 deaths. Here are 
     the top countries in this indicator:
-    
+
     ### 15 countries which solved the problem of mortality from СOVID-19 the best of all
     """
     st.write(triple_df.sort_values(by=['slope'])[:15].reset_index().drop(['index'], axis=1))
@@ -100,37 +106,40 @@ with st.echo(code_location='below'):
     population_list = []
     for i in range(2, 243):
         population_list.append([soup.find(class_="wikitable").find_all("tr")[i].find('td').text.strip(),
-                                int(soup.find(class_="wikitable").find_all("tr")[i].find_all('td')[
-                                        2].text.strip().replace(",", ""))])
+                                float(soup.find(class_="wikitable").find_all("tr")[i].find_all('td')[
+                                          2].text.strip().replace(",", "").replace("%", ""))])
     population_df = pd.DataFrame(population_list, columns=['country', 'population'])
 
     df = df.merge(population_df, left_on='location', right_on='country', how='left').drop(['country'], axis=1)
 
     df_without_nan = df.copy()
     df_without_nan = df_without_nan.dropna()
+    # df_without_nan['population'] = df_without_nan['population'].replace(0, 0.01)
 
     countries_for_vaccines = list(df_without_nan['location'].unique())
     list_perc_of_deaths_and_vaccines = []
     for country in countries_for_vaccines:
         list_perc_of_deaths_and_vaccines.append([country,
                                                  100 * int(
-                                                     df[df['location'].isin([country])].tail(1)['total_deaths']) / int(
+                                                     df[df['location'].isin([country])].tail(1)[
+                                                         'total_deaths']) / float(
                                                      df_without_nan[df_without_nan['location'].isin([country])].tail(1)[
                                                          'population']),
                                                  100 * int(
                                                      df_without_nan[df_without_nan['location'].isin([country])].tail(1)[
-                                                         'people_fully_vaccinated']) / int(
+                                                         'people_fully_vaccinated']) / float(
                                                      df_without_nan[df_without_nan['location'].isin([country])].tail(1)[
                                                          'population']),
                                                  100 * int(
                                                      df_without_nan[df_without_nan['location'].isin([country])].tail(1)[
-                                                         'total_cases']) / int(
+                                                         'total_cases']) / float(
                                                      df_without_nan[df_without_nan['location'].isin([country])].tail(1)[
                                                          'population'])
                                                  ])
 
     mortality_and_vaccinated_df = pd.DataFrame(list_perc_of_deaths_and_vaccines,
-                                               columns=['country', 'perc. of mortality', 'perc. of vacc', 'perc. of morbidity'])
+                                               columns=['country', 'perc. of mortality', 'perc. of vacc',
+                                                        'perc. of morbidity'])
     mortality_and_vaccinated_df = mortality_and_vaccinated_df.merge(triple_df, left_on='country', right_on='country',
                                                                     how='left')
 
@@ -147,10 +156,11 @@ with st.echo(code_location='below'):
     sns.set(font_scale=0.8)
     sns.set(rc={'figure.figsize': (15,
                                    len(list(mortality_and_vaccinated_df[mortality_and_vaccinated_df['continent'] ==
-                                                                        option_continents ]['country'])) / 3)})
+                                                                        option_continents]['country'])) / 3)})
     ax = sns.barplot(x="perc. of vacc", y="country",
-                data=mortality_and_vaccinated_df[mortality_and_vaccinated_df['continent'] == option_continents].sort_values(
-                    by=['perc. of vacc']))
+                     data=mortality_and_vaccinated_df[
+                         mortality_and_vaccinated_df['continent'] == option_continents].sort_values(
+                         by=['perc. of vacc']))
     ax.set_title(option_continents, fontsize=40)
     ax.set_xlabel('Percentage of population that has been vaccinated', fontsize=20);
     ax.set_ylabel('Country', fontsize=20);
@@ -181,7 +191,7 @@ with st.echo(code_location='below'):
     df1 = df1[['Country', 'Lat', 'Long']]
     mortality_and_vaccinated_df = mortality_and_vaccinated_df.merge(df1, left_on='country', right_on='Country',
                                                                     how='left')
-    
+
     ### FROM: "https://www.youtube.com/watch?v=GyMO9WCEheQ&t=170s"
     morbid_list = list(mortality_and_vaccinated_df['perc. of morbidity'])
     circle_radius = interp1d([0, max(morbid_list)], [5, 18])(morbid_list)
@@ -192,7 +202,7 @@ with st.echo(code_location='below'):
                                     'perc. of morbidity': '% of morbidity'},
                             title='Percentage of people infected with COVID-19 by 2022', width=700, height=550)
     ### END FROM
-    
+
     st.plotly_chart(fig)
 
     """
@@ -201,17 +211,6 @@ with st.echo(code_location='below'):
     2) https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases
     3) https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population
     """
-
-
-
-
-
-
-
-
-
-
-
 
 
 
